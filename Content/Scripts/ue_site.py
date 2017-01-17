0685f7d38ed33670ba3efcbc18bbeb78fdb2c806
getnamo <getnamo@gmail.com>
2017-01-17 23:08:18 +0000

    add sys path for plugins

---
+++ Content/Scripts/ue_site.py
@@ -0,0 +1,24 @@
+import sys
+import unreal_engine as ue
+
+#get the relative paths
+content = ue.get_content_dir()
+
+pbin = content + "../Plugins/UnrealEnginePython/Binaries/Win64"
+psite = pbin + "/Lib/site-packages"
+#unreal_engine.get_content_dir()
+
+pbinf = ue.convert_relative_path_to_full(pbin)
+psitef = ue.convert_relative_path_to_full(psite)
+
+#pip modules
+sys.path.insert(0,pbinf)
+sys.path.insert(0,psitef)
+ue.log('Added py script path: ' + pbinf)
+ue.log('Added py script path: ' + psitef)
+
+#plugin modules - TF
+tfscript = content + "../Plugins/tensorflow-ue4/Content/Scripts"
+tfscriptf = ue.convert_relative_path_to_full(tfscript)
+ue.log('Added py script path: ' + tfscriptf)
+sys.path.insert(0,tfscriptf)
