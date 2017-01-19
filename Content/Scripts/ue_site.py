328ff8d50175994f52b4036e946dac2802085df0
getnamo <getnamo@gmail.com>
2017-01-19 02:40:15 +0000

    comment ue_site python code
    -It's all handled in C++ now for all plugins

--- Content/Scripts/ue_site.py
+++ Content/Scripts/ue_site.py
@@ -1,15 +1,17 @@
-import sys
-import unreal_engine as ue
+#We managed to move all this into plugins! keep for commented for now
+
+#import sys
+#import unreal_engine as ue
 
 #get the relative paths
-content = ue.get_content_dir()
+#content = ue.get_content_dir()
 
-pbin = content + "../Plugins/UnrealEnginePython/Binaries/Win64"
-psite = pbin + "/Lib/site-packages"
+#pbin = content + "../Plugins/UnrealEnginePython/Binaries/Win64"
+#psite = pbin + "/Lib/site-packages"
 #unreal_engine.get_content_dir()
 
-pbinf = ue.convert_relative_path_to_full(pbin)
-psitef = ue.convert_relative_path_to_full(psite)
+#pbinf = ue.convert_relative_path_to_full(pbin)
+#psitef = ue.convert_relative_path_to_full(psite)
 
 #pip modules - these are now handled in c++! woohoo
 #sys.path.insert(0,pbinf)
@@ -18,10 +20,7 @@ psitef = ue.convert_relative_path_to_full(psite)
 #ue.log('Added py script path: ' + psitef)
 
 #plugin modules - TF - todo: move from project to plugin
-tfscript = content + "../Plugins/tensorflow-ue4/Content/Scripts"
-tfscriptf = ue.convert_relative_path_to_full(tfscript)
-ue.log('Added py script path: ' + tfscriptf)
-sys.path.insert(0,tfscriptf)
-
-ue.log('sys path is: ')
-ue.log(sys.path)
+#tfscript = content + "../Plugins/tensorflow-ue4/Content/Scripts"
+#tfscriptf = ue.convert_relative_path_to_full(tfscript)
+#ue.log('Added py script path: ' + tfscriptf)
+#sys.path.insert(0,tfscriptf)
