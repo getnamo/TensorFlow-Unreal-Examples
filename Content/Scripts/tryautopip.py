c8babd7a3fc4121d5f1b4a737d5e381590187dc9
getnamo <getnamo@gmail.com>
2017-02-05 18:55:15 +0000

    redirector moved to plugin content

--- Content/Scripts/tryautopip.py
+++ Content/Scripts/tryautopip.py
@@ -3,21 +3,7 @@
 import pip
 import sys
 import unreal_engine as ue
-
-class Redirector(object):
-    def __init__(self):
-        pass
-
-    def write(self, message):
-        ue.log(message)
-
-    def flush(self):
-    	sys.stdout.flush
-    def splitlines(self):
-    	sys.stdout.splitlines
-
-sys.stdout = Redirector()
-#sys.stderr = Redirector()
+import redirect_print
 
 #now we're ready, try to auto import pytest
 try:
