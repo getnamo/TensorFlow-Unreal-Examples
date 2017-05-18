c58fee9d8dc2af5759aaad689f3d78f05dd34ee4
getnamo <getnamo@gmail.com>
2017-05-18 12:42:47 +0100

    cleanup simple and add a tensorboard example

--- Content/Scripts/mnistSimpleBoard.py
+++ Content/Scripts/mnistSimpleBoard.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import argparse
 import sys
 import operator
+import datetime
 
 # Import data
 from tensorflow.examples.tutorials.mnist import input_data
@@ -12,16 +13,42 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import unreal_engine as ue
 
+this = sys.modules[__name__]
+this.shouldstop = False
+
+def variable_summaries(var):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
+
 def main(data_dir):
 	mnist = input_data.read_data_sets(data_dir, one_hot=True)
 
 	ue.log("starting mnist simple")
 
 	# Create the model
-	x = tf.placeholder(tf.float32, [None, 784])
-	W = tf.Variable(tf.zeros([784, 10]))
-	b = tf.Variable(tf.zeros([10]))
-	y = tf.matmul(x, W) + b
+	with tf.name_scope('x'):
+		x = tf.placeholder(tf.float32, [None, 784])
+		variable_summaries(x)
+
+	with tf.name_scope('weights'):
+		W = tf.Variable(tf.zeros([784, 10]))
+		variable_summaries(W)
+
+	with tf.name_scope('b'):
+		b = tf.Variable(tf.zeros([10]))
+		variable_summaries(b)
+
+	with tf.name_scope('b'):
+		y = tf.matmul(x, W) + b
+		variable_summaries(y)
 
 	# Define loss and optimizer
 	y_ = tf.placeholder(tf.float32, [None, 10])
@@ -36,37 +63,60 @@ def main(data_dir):
 	# So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
 	# outputs of 'y', and then average across the batch.
 	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+
 	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
+	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	tf.summary.scalar('accuracy', accuracy)
+
 	sess = tf.InteractiveSession()
+
+	#save stuff
+	merged = tf.summary.merge_all()
+	writer = tf.summary.FileWriter("/tmp/logs/mnist/" + str(datetime.datetime.now().strftime("%Y.%m.%d-%I.%M.%S")), graph=tf.get_default_graph())
+
 	# Train
 	tf.global_variables_initializer().run()
 	for i in range(1000):
+		if(this.shouldstop):
+			break
+
 		batch_xs, batch_ys = mnist.train.next_batch(100)
-		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-		if i % 100 == 0: 
+		_, summary = sess.run([train_step, merged], feed_dict={x: batch_xs, y_: batch_ys})
+		if i % 50 == 0:
+			writer.add_summary(summary, i)
 			ue.log(i)
+		else:
+			sess.run([train_step], feed_dict={x: batch_xs, y_: batch_ys})
 
 	# Test trained model
 	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+	tf.summary.scalar('accuracy', accuracy)
+
 	finalAccuracy = sess.run(accuracy, feed_dict={x: mnist.test.images,
 									  y_: mnist.test.labels})
 	ue.log('final training accuracy: ' + str(finalAccuracy))
 	
 	#return trained model
-	model = {'x':x, 'y':y, 'W':W,'b':b}
-	stored =  {'model': model, 'session':sess}
+	stored = {'x':x, 'y':y, 'W':W,'b':b, 'session':sess}
 
-	#ue.log('trained x: ' + str(model['x']))
-	#ue.log('trained y: ' + str(model['y']))
-	#ue.log('trained W: ' + str(model['W']))
-	#ue.log('trained b: ' + str(model['b']))
+	#ue.log('trained x: ' + str(stored['x']))
+	#ue.log('trained y: ' + str(stored['y']))
+	#ue.log('trained W: ' + str(stored['W']))
+	#ue.log('trained b: ' + str(stored['b']))
 
 	#store optional summary information
 	stored['summary'] = {'x':str(x), 'y':str(y), 'W':str(W), 'b':str(b)}
 
 	return stored
+
+def runModelWithInput(session, model, input_dict):
+	return session.run(model['y'], input_dict)
 
 #expected api: storedModel and session, json inputs
 def runJsonInput(stored, jsonInput):
@@ -74,14 +124,14 @@ def runJsonInput(stored, jsonInput):
 	pixelarray = jsonInput['pixels']
 	ue.log('image len: ' + str(len(pixelarray)))
 
-	#run our model
-	sess = stored['session']
-	model = stored['model']
-
 	#embedd the input image pixels as 'x'
-	feed_dict = {model['x']: [pixelarray]}
+	feed_dict = {stored['x']: [pixelarray]}
 
-	result = sess.run(model['y'], feed_dict)
+	#ue.log(feed_dict)
+
+	#run our model
+	result = runModelWithInput(stored['session'], stored, feed_dict)
+	ue.log('result: ' + str(len(pixelarray)))
 
 	#convert our raw result to a prediction
 	index, value = max(enumerate(result[0]), key=operator.itemgetter(1))
@@ -97,3 +147,7 @@ def runJsonInput(stored, jsonInput):
 def train():
 	data_dir = '/tmp/tensorflow/mnist/input_data'
 	return main(data_dir)
+
+#expected api: early stopping
+def stop():
+	this.shouldstop = True
